045ba8bffecbf6a3abbd7c32771fc88ef93d149e
Thomas <Thomasbronsveld94@gmail.com>
2019-09-09 18:31:33 +0200

    Removed duplicate functionality

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ class Block{
     }
     
     calculateHash(){
+        this.data = sha256(this.data);
         this.hash = sha256(this.data, this.index, this.timestamp, this.prevHash);
     }
 }
@@ -39,7 +40,7 @@ class Blockchain {
     }
     createGenesisBlock(){
         let genesisBlock = new Block(this.chain.length);
-        genesisBlock.data = sha256(memeList[genesisBlock.index]);
+        genesisBlock.data.push(memeList[genesisBlock.index]);
         genesisBlock.prevHash = null;
         genesisBlock.calculateHash(genesisBlock.data);
         this.chain.push(genesisBlock);
@@ -66,7 +67,7 @@ setInterval(function(){
 
 //Voeg nieuwe Block toe.
 let testBlock = new Block(blockChain.chain.length);
-testBlock.data.push(sha256(memeList[testBlock.index]));
+testBlock.data.push(memeList[testBlock.index]);
 testBlock.prevHash = blockChain.chain[blockChain.chain.length - 1].hash;
 testBlock.calculateHash(testBlock.data);
 blockChain.addBlock(testBlock);
@@ -78,7 +79,7 @@ console.log("test is geweest");
 
 //3de block.
 let testBlock3 = new Block(blockChain.chain.length);
-testBlock3.data.push(sha256(memeList[testBlock3.index]));
+testBlock3.data.push(memeList[testBlock3.index]);
 testBlock3.prevHash = blockChain.chain[blockChain.chain.length - 1].hash;
 testBlock3.calculateHash(testBlock3.data);
 blockChain.addBlock(testBlock3);
